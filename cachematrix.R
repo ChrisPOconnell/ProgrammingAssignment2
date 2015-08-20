3e31798351c2b3359dc9c0badfec1c76a60845b7
Chris P. OConnell <chrispoconnell@outlook.com>
2015-08-20 15:58:58 -0400

    Test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Chris P. OConnell
+##8/20/2015
 
-## Write a short comment describing this function
+##I've made the makeCacheMatrix really nice for you.
+##No need to send any complicated argument in.  Just 
+##type in the numbers you want in the matrix.
+##I'll check to see if the matrix can be square.
+##If the numbers can't create a square matrix I'll
+##let you know.
+##example:  mat <- makeCacheMatrix(1:100)
+##In this case I will create a nice 10x10 matrix for you.
 
 makeCacheMatrix <- function(x = matrix()) {
+  len <- length(x)                  #Determines length of x.
+                                    #Used for two things:
+                                    #1st, determine if matrix can be square
+                                    #2nd, create  sq
+  sq <- sqrt(len)           
+  mod <- sqrt(len) %% 1              #This determines if the matrix can be square
+  if (mod!=0){
+    print("Sorry, the values you provided can't create a square matrix.")
+  }
+  else {
+  #if (mod == 0 && len!=0) { #If matrix can be square, enter perform if statement.
+      mt<<- matrix(x,nrow=sq,ncol=sq) #using the square root of the values you provide
+                                      #a square matrix 
+      xvalues<<-x
+      #print("Congrats, your thingy is created")
+      
+      getmat <- function() print(mt)
+      setinvm <- function() invm <<- solve(mt)    #inverses mt, no need to
+                                                  #functions as an argument.
+                                                  #much less confusing!
+      getinvm <- function() invm                  #shows inversed mt
+      getxvalues <- function() print(xvalues)
 
-}
-
-
-## Write a short comment describing this function
-
+  }
+  
+  multiplymatrixes <- function() {
+    mni <- getmat()
+    mi <- getinvm()
+    mi %*% mni
+  }
+      
+      list(setinvm = setinvm,
+           getmat = getmat,
+           getinvm = getinvm,
+           getxvalues = getxvalues,
+           multiplymatrixes = multiplymatrixes)
+  }      
+  
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
 }
