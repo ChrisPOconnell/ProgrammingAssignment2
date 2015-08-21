7adaaffc83495024d82337e05b20bfafcbec24a9
Chris P. OConnell <chrispoconnell@outlook.com>
2015-08-21 11:26:39 -0400

    adding comments and cleaning up leftover debug code.

--- cachematrix.R
+++ cachematrix.R
@@ -12,41 +12,40 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   invm <- NULL                      #This is required to prevent a previously defined
-  #invm from returning and falsey being reported
-  #as the inversion of our current matrix.
-  len <- length(x)                  #Determines length of x.
-  #Used for two things:
-  #1st, determine if matrix can be square
-  #2nd, create  sq
+                                    #invm from returning and falsey being reported
+                                    #as the inversion of our current matrix.
+  len <- length(x)                  #Determines length of x (the argument passed into function).
+                                    #Used for two things:
+                                    #1st, determine if matrix can be square
+                                    #2nd, create  sq
   sq <- sqrt(len)           
-  mod <- sqrt(len) %% 1              #This determines if the matrix can be square
+  mod <- sqrt(len) %% 1             #This determines if the matrix can be square
   if (mod!=0){
     print("Sorry, the values you provided can't create a square matrix.")
   }
   else {
-    #if (mod == 0 && len!=0) { #If matrix can be square, enter perform if statement.
     mt<- matrix(x,nrow=sq,ncol=sq) #using the square root of the values you provide
-    #a square matrix 
+                                   #a square matrix 
     xvalues<<-x
-    #print("Congrats, your thingy is created")
-    
+
     getmat <- function() return(mt)
     setinvm <- function(matvalue) invm <<- solve(matvalue)
     
     
     getinvm <- function() invm                
     getxvalues <- function() print(xvalues)
-    
   }
   
   #  I wasn't able to get this function working, but was attempting to 
-  #  create a function 
+  #  create a function that multiplies the inverse and the regular matrix.
   multiplymatrixes <- function() {
     mni <- getmat()
     mi <- getinvm()
     mi %*% mni
   }
   
+  #This code sets what functions are available for the object created with
+  #makeCacheMatrix
   list(setinvm = setinvm,
        getmat = getmat,
        getinvm = getinvm,
